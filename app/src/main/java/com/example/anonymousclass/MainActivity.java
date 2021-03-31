cfa8357372b57ac17b7f6d27c77dd53d0466cf9f
murod1 <jmurod2000@gmail.com>
2021-03-31 13:48:24 +0900

    Anonymous Class

--- app/src/main/java/com/example/anonymousclass/MainActivity.java
+++ app/src/main/java/com/example/anonymousclass/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mButton1 ;
+    Button mButton1;
     TextView mTextView1;
 
 
@@ -21,14 +21,13 @@ public class MainActivity extends AppCompatActivity {
         mButton1 = findViewById(R.id.button1);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener());
+        mButton1.setOnClickListener(new View.OnClickListener() {
 
-    }
+            @Override
+            public void onClick(View v) {
+                mTextView1.setText("You clicked button 1");
+            }
+        });
 
-    private class MyOnClickListener  implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            mTextView1.setText("You clicked button 1");
-        }
     }
 }
